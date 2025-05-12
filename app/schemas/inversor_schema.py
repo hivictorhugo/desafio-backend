fa7e5e53465392a12413a6d1ef5b836974663708
hivictorhugo <vhqueirozcouto@gmail.com>
2025-05-12 13:29:01 -0300

    Fixing Schemas

--- app/schemas/inversor_schema.py
+++ app/schemas/inversor_schema.py
@@ -4,3 +4,5 @@ class InversorSchema(Schema):
     id = fields.Int(dump_only=True)
     nome = fields.Str(required=True)
     usina_id = fields.Int(required=True)
+    potencia_maxima = fields.Float(required=True)
+    temperatura = fields.Float(allow_none=True)
--- app/schemas/usina_schema.py
+++ app/schemas/usina_schema.py
@@ -1,6 +1,13 @@
 from marshmallow import Schema, fields
+from app.schemas.inversor_schema import InversorSchema
 
 class UsinaSchema(Schema):
     id = fields.Int(dump_only=True)
     nome = fields.Str(required=True)
     localizacao = fields.Str()
+
+    inversores = fields.Nested(InversorSchema, many=True, dump_only=True)
+
+    
+
+  
